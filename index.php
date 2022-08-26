24c60782c6979cd093ab5a39f33df8bdfb065588
raissa <r.tenorio@aluno.ifsp.edu.br>
2022-08-26 10:40:56 -0300

    rew

--- index.php
+++ index.php
@@ -79,7 +79,31 @@
 </div>
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-      
+
+<div class="container marketing">
+
+<!-- Três colunas de texto, abaixo do carousel -->
+<div class="row">
+  <div class="col-lg-4">
+    <img class="rounded-circle" src="img/cu.png" alt="Generic placeholder image" width="140" height="140">
+    <h2>R&W</h2>
+    <p>Read and write: take your own time</p>
+    <p><a class="btn btn-secondary" href="#" role="button">Saber mais &raquo;</a></p>
+  </div><!-- /.col-lg-4 -->
+  <div class="col-lg-4">
+    <img class="rounded-circle" src="img/cu2.png" alt="Generic placeholder image" width="140" height="140">
+    <h2>Título</h2>
+    <p>Leia quando quiser e onde quiser</p>
+    <p><a class="btn btn-secondary" href="#" role="button">Saber mais &raquo;</a></p>
+  </div><!-- /.col-lg-4 -->
+  <div class="col-lg-4">
+    <img class="rounded-circle" src="img/cu3.png" alt="Generic placeholder image" width="140" height="140">
+    <h2>Fanart</h2>
+    <p>Solte sua imaginação: crie histórias e publique ilustrações</p>
+    <p><a class="btn btn-secondary" href="#" role="button">Saber mais &raquo;</a></p>
+  </div><!-- /.col-lg-4 -->
+</div><!-- /.row -->
+
 </div>
 <div class="row">
   <div class="col-md-12">
