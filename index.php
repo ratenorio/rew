9db7845cebb7b527da45c1a9efe4bda8fa67749c
raissa <r.tenorio@aluno.ifsp.edu.br>
2022-09-21 12:22:00 -0300

    rew

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
         <div class="col-lg-4">
           <img class="rounded-circle" src="img/saber1.png" alt="Generic placeholder image" width="140" height="140">
           <div class="texto3">
-            <h2 class="text-center" >Livros</h2>
+            <h2 class="text-center">Livros</h2>
             <p class="text-center">Leia e escreva: leve o tempo que precisar!</p>
             <p><a class="btn btn-dark btn-left" href="#livros" role="button">Saber mais</a></p>
           </div>
@@ -116,14 +116,16 @@
 
   <!--saber mais-->
 
-  <hr class="featurette-divider">
+  <hr class="featurette-divider" style="background-color: rgb(163, 232, 255);">
 
-        <div class="row featurette">
+      <div class="row featurette" style="background-color: rgb(163, 232, 255);">
           <div class="col-md-7">
+            <div class="teste">
             <h2 class="featurette-heading" id="livros">Livros</h2>
             <p class="lead">Essa plataforma permite que você possa ler e se tornar autor de suas próprias histórias.<br>
             Já viram as nossas coleções? <br>
             Clique aqui e não perca mais tempo.</p>
+            </div>
           </div>
 
           <!--livros carrousel-->
@@ -132,13 +134,13 @@
               <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img class="d-block w-50" src="img/capa.jpeg" alt="Primeiro Slide" style="height: 600px; ">
+                    <img class="d-block w-50" src="img/capa.jpeg" alt="First slide" style="height: 600px; ">
                   </div>
                   <div class="carousel-item">
-                    <img class="d-block w-50" src="img/capa2.jpeg" alt="Segundo Slide" style="height: 600px; ">
+                    <img class="d-block w-50" src="img/capa2.jpeg" alt="Second slide" style="height: 600px; ">
                   </div>
                   <div class="carousel-item">
-                    <img class="d-block w-50" src="img/capa3.jpeg" alt="Terceiro Slide" style="height: 600px; ">
+                    <img class="d-block w-50" src="img/capa3.jpeg" alt="Third slide" style="height: 600px; ">
                   </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -154,15 +156,17 @@
           </div>
         </div>
 
-        <hr class="featurette-divider">
+        <hr class="featurette-divider"style="background-color: rgb(163, 232, 255);">
 
-        <div class="row featurette">
+        <div class="row featurette" style="background-color: rgb(163, 232, 255);">
           <div class="col-md-7 order-md-2">
             <div class="vertical">
-              <h2 class="featurette-heading" id="rew">R&W</h2>
-              <p class="lead">Só acha a leitura perda de tempo quem ainda não aprendeu a viver com qualidade.<br>
-              Aqui você poderá ler e escrever onde quiser e quando quiser.<br> 
-              Para ler offline, baixe as histórias e aproveite.</p>
+              <div class="teste2">
+                <h2 class="featurette-heading" id="rew">R&W</h2>
+                <p class="lead">Só acha a leitura perda de tempo quem ainda não aprendeu a viver com qualidade.<br>
+                Aqui você poderá ler e escrever onde quiser e quando quiser.<br> 
+                Para ler offline, baixe as histórias e aproveite.</p>
+              </div>
             </div>
           </div>
           <div class="col-md-5 order-md-1">
@@ -172,16 +176,18 @@
           </div>
         </div>
 
-        <hr class="featurette-divider">
+        <hr class="featurette-divider" style="background-color: rgb(163, 232, 255);">
 
-        <div class="row featurette">
+        <div class="row featurette" style="background-color: rgb(163, 232, 255);">
           <div class="col-md-7">
-            <h2 class="featurette-heading" id="fanart">Fanart</h2>
-            <p class="lead">Ler é formular imagens invisíveis.<br>
-            Você já se prendeu tanto a uma história que sentiu como se estivesse vivendo-a?<br>
-             Já imaginou os personagens e diferentes cenários presentes nas narrativas?<br>
-            Não deixe tudo apenas na sua imaginação, faça ilustrações e publique em nossa plataforma.<br>
-            Para publicar e ver as fanarts, acesse o livro escolhido e entre nos comentários.</p>
+            <div class="teste3">
+              <h2 class="featurette-heading" id="fanart">Fanart</h2>
+              <p class="lead">Ler é formular imagens invisíveis.<br>
+              Você já se prendeu tanto a uma história que sentiu como se estivesse vivendo-a?<br>
+              Já imaginou os personagens e diferentes cenários presentes nas narrativas?<br>
+              Não deixe tudo apenas na sua imaginação, faça ilustrações e publique em nossa plataforma.<br>
+              Para publicar e ver as fanarts, acesse o livro escolhido e entre nos comentários.</p>
+            </div>
           </div>
           <div class="col-md-5">
             <div class="container2">
@@ -191,6 +197,7 @@
         </div>
 
         <hr class="featurette-divider">
+      </div>
 
   <!-- Rodapé da página -->
   <?php
